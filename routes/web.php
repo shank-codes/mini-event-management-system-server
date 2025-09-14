a23dc92d97897ee2abe0ec6c4633c66a41dc2143
shank-codes <shashankhiremat22@gmail.com>
2025-09-14 18:44:51 +0530

    initial setup

---
+++ routes/web.php
@@ -0,0 +1,13 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\DB;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+
+Route::get('/db-test', function () {
+    return DB::select('SELECT version()');
+});
